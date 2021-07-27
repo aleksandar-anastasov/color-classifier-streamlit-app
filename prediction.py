6afb0f847e8dc2495920382eb0e77ae2dd8b17e6
aleksandar-anastasov <anastasoff2002@gmail.com>
2021-07-27 20:03:53 +0200

    add prediction.py

--- prediction.py
+++ prediction.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f10ab81a1243c85dc962fa4b89f9c97e3c70a9ec024a4ca49be89730b67a29c2
-size 1541
+oid sha256:efd17555b4c7d52f75c3c2c621bc1a7ecf966443f8a2ebe2e4a8aa49ac1b66ed
+size 2215
